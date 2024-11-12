36901945825fed41a457914649e7bc62d94c3147
AKSHAT2802 <akshat.kakkad2802@gmail.com>
2024-11-13 01:40:12 +0530

    Remove default colour pallete

--- packages/block-editor/src/components/color-palette/with-color-context.js
+++ packages/block-editor/src/components/color-palette/with-color-context.js
@@ -11,19 +11,13 @@ import { useSettings } from '../use-settings';
 export default createHigherOrderComponent( ( WrappedComponent ) => {
 	return ( props ) => {
 		// Get the default colors, theme colors, and custom colors
-		const [ defaultColors, themeColors, customColors, enableCustomColors ] =
-			useSettings(
-				'color.palette.default',
-				'color.palette.theme',
-				'color.palette.custom',
-				'color.custom'
-			);
+		const [ themeColors, customColors, enableCustomColors ] = useSettings(
+			'color.palette.theme',
+			'color.palette.custom',
+			'color.custom'
+		);
 
-		const colors = [
-			...( defaultColors || [] ),
-			...( themeColors || [] ),
-			...( customColors || [] ),
-		];
+		const colors = [ ...( themeColors || [] ), ...( customColors || [] ) ];
 
 		const { disableCustomColors = ! enableCustomColors } = props;
 
